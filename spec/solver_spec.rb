df0726e77d90e493e018891d9a118004ecd63c2b
tafaramafemba <tafara.maf@gmail.com>
2022-06-23 20:23:55 +0200

    factorial method tests

---
+++ spec/solver_spec.rb
@@ -0,0 +1,23 @@
+require_relative 'spec_helper'
+
+RSpec.describe Solver do
+  # context 'checks to make sure that Solver is initialized' do
+ 
+  # end
+  context 'Testing the factorial method' do
+
+    it 'Checks that the factorial method can receive a value' do
+      mock = double('solver')
+      allow(mock).to receive(:factorial).with (0)
+
+      solver = Solver.new()
+      solver.factorial(1)
+    end
+
+    it "Checks that the factorial method identifies 0 as an argument and returns 1" do
+      solver = Solver.new()
+      expect(solver.factorial(0)).to eql 1
+    end
+
+  end
+end
